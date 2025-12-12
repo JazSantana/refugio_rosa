1a9f4c4e763306db0afd798cffbd5f62a37d09e8
JazSantanaCTC <marsantanacuzcano@crackthecode.la>
2025-12-12 10:48:17 -0500

    Titulos

--- src/pages/Autocuidado.jsx
+++ src/pages/Autocuidado.jsx
@@ -61,7 +61,7 @@ function Autocuidado() {
     <section className="autocuidado">
       <div >
         <h2 className=" m-6 text-4xl ips titulo">
-          <>♔Tips para ti♔</>
+          <>Tips para ti</>
         </h2>
 
         <div className="row" id="tarjetas-container">
